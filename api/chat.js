32018374166a1536bf9b0bc1e79f154e48f39635
myselfneerajnaiyar-hash <myselfneerajnaiyar@gmail.com>
2025-11-26 02:37:07 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,10 +1,1 @@
-export default function handler(req, res) {
-  const userMessage =
-    (req.query && req.query.message) ||
-    (req.body && req.body.message) ||
-    "no message received";
-
-  res.status(200).json({
-    reply: Echo from TatvaBot: ${userMessage},
-  });
-}
+// disabled
