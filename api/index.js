1a11924bde7e3ce008978352a614c95fe34027a8
myselfneerajnaiyar-hash <myselfneerajnaiyar@gmail.com>
2025-11-26 02:08:31 +0530

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  const userMessage =
+    (req.query && req.query.message) ||
+    (req.body && req.body.message) ||
+    "no message received";
+
+  res.status(200).json({
+    reply: Echo from TatvaBot NEW: ${userMessage},
+  });
+}
